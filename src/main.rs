55211085180132b4a827490bfe65bc18c65a34f1
varunsindwani <varun@sindwani.com>
2020-06-27 19:13:10 -0400

    code split into diff mods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod lexer;
+use lexer::{lexer};
+#[warn(unused_imports)]
+
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("test/main.rao");
+    let display = path.display();
+
+    let mut file = match File::open(&path)  {
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s)   {
+        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+        Ok(_) => println!(""),//print!("{} contains: \n{}", display, s), 
+    }
+    lexer(s)
 }
+
